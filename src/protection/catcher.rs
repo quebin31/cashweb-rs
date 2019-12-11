448c0b39f371b3dac9b0f5e7e9d249628ab8c756
Harry Barber <harrybarber@protonmail.com>
2019-12-11 22:28:25 +0700

    Add static redirect

--- src/protection/catcher.rs
+++ src/protection/catcher.rs
@@ -1,10 +1,13 @@
+use std::marker::PhantomData;
+
 use super::{GuardError, ProtectedService};
 
 use futures::{
+    future,
     prelude::*,
     task::{Context, Poll},
 };
-use http::Request;
+use http::{Request, Response};
 use tower_layer::Layer;
 use tower_service::Service;
 
@@ -68,5 +71,49 @@ where
 
     fn layer(&self, service: ProtectedService<S, T, V>) -> Self::Service {
         ProtectedCatcher::new(service, self.catcher.clone())
+    }
+}
+
+#[derive(Clone)]
+pub struct StaticRedirect<S, V, B> {
+    redirect_url: String,
+    service_error: PhantomData<S>,
+    validation_error: PhantomData<V>,
+    body: PhantomData<B>,
+}
+
+impl<S, V, B> StaticRedirect<S, V, B> {
+    pub fn new(redirect_url: &str) -> Self {
+        StaticRedirect {
+            redirect_url: redirect_url.to_string(),
+            service_error: PhantomData::<S>::default(),
+            validation_error: PhantomData::<V>::default(),
+            body: PhantomData::<B>::default(),
+        }
+    }
+}
+
+impl<S, V, B> Service<GuardError<S, V>> for StaticRedirect<S, V, B>
+where
+    B: Default,
+    V: 'static,
+    B: 'static,
+    S: 'static,
+{
+    type Response = Response<B>;
+    type Error = GuardError<S, V>;
+    type Future = ResponseFuture<Self::Response, Self::Error>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, _: GuardError<S, V>) -> Self::Future {
+        Box::pin(future::ok(
+            Response::builder()
+                .header("Location", self.redirect_url.clone())
+                .body(B::default())
+                .unwrap(),
+        ))
     }
 }
