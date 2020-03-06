7c58cd30dd8996ad8adfad8c8996254b73cbe98c
Harry Barber <harrybarber@protonmail.com>
2020-03-06 19:42:31 +0700

    Include more protobufs

--- cashweb-protobuf/build.rs
+++ cashweb-protobuf/build.rs
@@ -1,8 +1,12 @@
 fn main() {
     prost_build::compile_protos(
         &[
-            "src/proto/addressmetadata.proto",
-            "src/proto/paymentrequest.proto",
+            "src/proto/pop/wrapper.proto",
+            "src/proto/pop/paymentrequest.proto",
+            "src/proto/keyserver/addressmetadata.proto",
+            "src/proto/relay/filters.proto",
+            "src/proto/relay/messaging.proto",
+            "src/proto/relay/stealth.proto"
         ],
         &["src/"],
     )
--- cashweb-protobuf/src/lib.rs
+++ cashweb-protobuf/src/lib.rs
@@ -2,6 +2,24 @@ pub mod address_metadata {
     include!(concat!(env!("OUT_DIR"), "/address_metadata.rs"));
 }
 
+pub mod wrapper {
+    include!(concat!(env!("OUT_DIR"), "/wrapper.rs"));
+}
+
+pub mod relay {
+    pub mod filters {
+        include!(concat!(env!("OUT_DIR"), "/filters.rs"));
+    }
+
+    pub mod messaging {
+        include!(concat!(env!("OUT_DIR"), "/messaging.rs"));
+    }
+
+    pub mod stealth {
+        include!(concat!(env!("OUT_DIR"), "/stealth.rs"));
+    }
+}
+
 pub mod bip70 {
     include!(concat!(env!("OUT_DIR"), "/bip70.rs"));
 }
