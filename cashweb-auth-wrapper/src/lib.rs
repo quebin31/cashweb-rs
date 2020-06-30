5ea08ff4ff347f0b3c54f56a934e1d47726d0a1f
Harry Barber <harrybarber@protonmail.com>
2020-06-30 17:04:42 +0100

    Improve documentation

--- cashweb-auth-wrapper/src/lib.rs
+++ cashweb-auth-wrapper/src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    rust_2018_idioms,
+    unreachable_pub
+)]
+#![allow(elided_lifetimes_in_paths)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+//! `cashweb-auth-wrapper` is a library providing deserialization, parsing, and verification needed within the [`Authorization Wrapper Framework`].
+//!
+//! [`Authorization Wrapper Framework`]: https://github.com/cashweb/specifications/blob/master/authorization-wrapper/specification.mediawiki
+
 mod models;
 use std::{convert::TryInto, fmt};
 
@@ -7,22 +20,34 @@ use secp256k1::{key::PublicKey, Error as SecpError, Message, Secp256k1, Signatur
 pub use models::{auth_wrapper::SignatureScheme, AuthWrapper};
 
 /// Represents a [`AuthWrapper`] post-parsing.
+#[derive(Debug, Clone)]
 pub struct ParsedAuthWrapper {
+    /// The public key associated with the signature.
     pub public_key: PublicKey,
+    /// The signature by public key covering the payload.
     pub signature: Signature,
+    /// The signature scheme used for signing.
     pub scheme: SignatureScheme,
+    /// The payload covered by the signature.
     pub payload: Vec<u8>,
+    /// The SHA256 digest of the payload.
     pub payload_digest: [u8; 32],
 }
 
 /// The error associated with validation and parsing of the [`AuthWrapper`].
 #[derive(Debug)]
 pub enum ParseError {
+    /// The public key provided was invalid.
     PublicKey(SecpError),
+    /// The signature provided was an invalid format.
     Signature(SecpError),
+    /// The signature scheme provided is unsupported.
     UnsupportedScheme,
+    /// The `payload_digest` provided was fraudulent.
     FraudulentDigest,
+    /// Both the digest and the payload were missing.
     DigestAndPayloadMissing,
+    /// The `payload_digest` was not 32 bytes long.
     UnexpectedLengthDigest,
 }
 
@@ -48,7 +73,7 @@ impl AuthWrapper {
     #[inline]
     pub fn parse(self) -> Result<ParsedAuthWrapper, ParseError> {
         // Parse public key
-        let public_key = PublicKey::from_slice(&self.pub_key).map_err(ParseError::PublicKey)?;
+        let public_key = PublicKey::from_slice(&self.public_key).map_err(ParseError::PublicKey)?;
 
         // Parse scheme
         let scheme = SignatureScheme::from_i32(self.scheme).ok_or(ParseError::UnsupportedScheme)?;
@@ -59,16 +84,16 @@ impl AuthWrapper {
         // Construct and validate payload digest
         let payload_digest = match self.payload_digest.len() {
             0 => {
-                if self.serialized_payload.is_empty() {
+                if self.payload.is_empty() {
                     return Err(ParseError::DigestAndPayloadMissing.into());
                 } else {
-                    let payload_digest = digest(&SHA256, &self.serialized_payload);
+                    let payload_digest = digest(&SHA256, &self.payload);
                     let digest_arr: [u8; 32] = payload_digest.as_ref().try_into().unwrap();
                     digest_arr
                 }
             }
             32 => {
-                let payload_digest = digest(&SHA256, &self.serialized_payload);
+                let payload_digest = digest(&SHA256, &self.payload);
                 if *payload_digest.as_ref() != self.payload_digest[..] {
                     return Err(ParseError::FraudulentDigest.into());
                 }
@@ -83,7 +108,7 @@ impl AuthWrapper {
             scheme,
             signature,
             payload_digest,
-            payload: self.serialized_payload,
+            payload: self.payload,
         })
     }
 }
@@ -91,15 +116,15 @@ impl AuthWrapper {
 /// Error associated with verifying the signature of an [`AuthWrapper`].
 #[derive(Debug)]
 pub enum VerifyError {
-    Message(SecpError),
+    /// The signature failed verification.
     InvalidSignature(SecpError),
+    /// The signature scheme provided is unsupported.
     UnsupportedScheme,
 }
 
 impl fmt::Display for VerifyError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::Message(err) => err.fmt(f),
             Self::InvalidSignature(err) => err.fmt(f),
             Self::UnsupportedScheme => f.write_str("unsupported signature scheme"),
         }
@@ -116,7 +141,7 @@ impl ParsedAuthWrapper {
         }
         // Verify signature on the message
         let msg =
-            Message::from_slice(self.payload_digest.as_ref()).map_err(VerifyError::Message)?;
+            Message::from_slice(self.payload_digest.as_ref()).unwrap(); // This is safe
         let secp = Secp256k1::verification_only();
         secp.verify(&msg, &self.signature, &self.public_key)
             .map_err(VerifyError::InvalidSignature)?;
