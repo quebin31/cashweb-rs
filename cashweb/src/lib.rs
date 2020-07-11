afc5ba2ca4fc5ff328a37a6c5ce56a2a045f658e
Harry Barber <harrybarber@protonmail.com>
2020-07-11 05:00:36 +0100

    Improve documentation

--- cashweb/src/lib.rs
+++ cashweb/src/lib.rs
@@ -1,10 +1,33 @@
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    rust_2018_idioms,
+    unreachable_pub
+)]
+
+//! `cashweb` is a collection of useful components, designed for quick integration of the following protocols:
+//! * [Proof-of-Payment Authorization Framework](https://github.com/cashweb/specifications/blob/master/proof-of-payment-token/specification.mediawiki)
+//! * [Authorization Wrapper Protocol](https://github.com/cashweb/specifications/blob/master/authorization-wrapper-protocol/specification.mediawiki)
+//! * [Keyserver Protocol](https://github.com/cashweb/specifications/blob/master/keyserver-protocol/specification.mediawiki)
+//! * [Relay Server Protocol](https://github.com/cashweb/specifications/blob/master/relay-server-protocol/specification.mediawiki)
+
+#[doc(inline)]
 pub use auth_wrapper;
+#[doc(inline)]
 pub use bitcoin;
+#[doc(inline)]
 pub use bitcoin_client;
+#[doc(inline)]
 pub use keyserver;
+#[doc(inline)]
 pub use keyserver_client;
+#[doc(inline)]
 pub use payments;
+#[doc(inline)]
 pub use relay;
+#[doc(inline)]
 pub use relay_client;
+#[doc(inline)]
 pub use secp256k1;
+#[doc(inline)]
 pub use token;
