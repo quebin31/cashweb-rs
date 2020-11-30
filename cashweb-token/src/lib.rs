9e5cf91d852e7d4b7e49c0e345cd65870028a768
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-11-30 08:56:58 -0800

    Add support for `access_token` query string as part of POP protocol
    Access_token needs to be supported in order for the websocket connection
    to success from a web browser. The API within web browsers cannot set
    custom headers. So, this is the workaround provided for now until
    the API for web browsers is updated somehow to allow for setting
    bearer tokens in the auth headers.
    
    See: https://tools.ietf.org/html/rfc6750#section-2.3

--- cashweb-token/src/lib.rs
+++ cashweb-token/src/lib.rs
@@ -16,20 +16,24 @@ use http::header::{HeaderMap, HeaderValue, AUTHORIZATION};
 /// Extract a POP token from `Authorization` header.
 pub fn extract_pop_header(value: &HeaderValue) -> Option<&str> {
     if let Ok(header_str) = value.to_str() {
-        if &header_str[..4] == "POP " {
-            Some(&header_str[4..])
-        } else {
-            None
-        }
-    } else {
-        None
+        return Some(header_str);
     }
+    None
+}
+
+fn split_pop_token(full_token: &str) -> Option<&str> {
+    if &full_token[..4] == "POP " {
+        return Some(&full_token[4..]);
+    }
+    None
 }
 
 /// Extract the first POP token from [`HeaderMap`].
-pub fn extract_pop(headers: &HeaderMap) -> Option<&str> {
+pub fn extract_pop<'a>(headers: &'a HeaderMap, query_token: &'a Option<String>) -> Option<&'a str> {
     headers
         .get_all(AUTHORIZATION)
         .iter()
         .find_map(extract_pop_header)
+        .or_else(|| query_token.as_ref().map(|token| &token[..]))
+        .and_then(split_pop_token)
 }
