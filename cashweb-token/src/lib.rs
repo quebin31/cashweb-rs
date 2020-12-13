1e29f00de559f11ed6e474768bd6ed3262858c32
Harry Barber <harrybarber@protonmail.com>
2020-12-13 11:26:20 +0000

    Remove breaking changes

--- cashweb-token/src/lib.rs
+++ cashweb-token/src/lib.rs
@@ -15,25 +15,43 @@ use http::header::{HeaderMap, HeaderValue, AUTHORIZATION};
 
 /// Extract a POP token from `Authorization` header.
 pub fn extract_pop_header(value: &HeaderValue) -> Option<&str> {
-    if let Ok(header_str) = value.to_str() {
-        return Some(header_str);
-    }
-    None
+    value.to_str().ok().and_then(split_pop_token)
 }
 
-fn split_pop_token(full_token: &str) -> Option<&str> {
-    if &full_token[..4] == "POP " {
-        return Some(&full_token[4..]);
+/// Split the POP token, removing the prefix "POP".
+pub fn split_pop_token(full_token: &str) -> Option<&str> {
+    if full_token.len() > 4 {
+        if &full_token[..4] == "POP " {
+            return Some(&full_token[4..]);
+        }
     }
     None
 }
 
 /// Extract the first POP token from [`HeaderMap`].
-pub fn extract_pop<'a>(headers: &'a HeaderMap, query_token: &'a Option<String>) -> Option<&'a str> {
+pub fn extract_pop(headers: &HeaderMap) -> Option<&str> {
     headers
         .get_all(AUTHORIZATION)
         .iter()
         .find_map(extract_pop_header)
-        .or_else(|| query_token.as_ref().map(|token| &token[..]))
-        .and_then(split_pop_token)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_split_ok() {
+        split_pop_token("POP abc").unwrap();
+    }
+
+    #[test]
+    fn test_split_short() {
+        assert_eq!(split_pop_token("A"), None);
+    }
+
+    #[test]
+    fn test_split_err() {
+        assert_eq!(split_pop_token("ABC d"), None);
+    }
 }
